f5379bbdaee16dc91b8bd235f50b6f1f9a5252ef
CryptoAutistic80 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-05-13 18:03:15 +0000

    update commit

--- config.py
+++ config.py
@@ -12,19 +12,19 @@ PINECONE_KEY = os.environ['PINECONE_KEY']
 MAX_RETRIES = 10
 
 # Set personlity temperature 0 = deterministic 1 = random and more creative
-PERSONALITY_TEMP = 0.8
+PERSONALITY_TEMP = 0.5
 
 # Set number of tokens the bot can use for the chat completion
-BOT_TOKENS = 400
+BOT_TOKENS = 500
 
 # Set the number of chat messages in the chat history
 CHAT_HISTORY = 12
 
 # Set time in secs of inactivity before coversation auto-closes
-CONVO_TIMEOUT = 300
+CONVO_TIMEOUT = 600
 
 # Set the maximum word limit for user messages
-MESSAGE_WORD_LIMIT = 80
+MESSAGE_WORD_LIMIT = 2000
 
 # Set the number of top similar documents to retrieve from Pinecone
-TOP_K_SIMILAR_DOCS = 8
+TOP_K_SIMILAR_DOCS = 5
